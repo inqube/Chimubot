9e57efe7fe06df9e96091f063b00922c491b5864
jimlarck <jimlarck@gmail.com>
2019-02-19 09:48:11 -0600

    base-updated
    token stored in private.json (MAKE SURE IT IS NEVER PUBLISHED). Command-related code goes in commands folder, then run in index.

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require("./config.json")
+const keys = require("./private.json");
 
 // Store token in config (DO NOT PASTE TOKEN OUTSIDE OF CONFIG)
-let token = config.token;
+let token = keys.token;
+var prefix = config.prefix;
 
+  // Return status
   client.once("ready", () => {
     console.log("Ready!");
   });
 
   client.login(token)
+
+  // RUN STUFF FROM COMMANDS HERE
+  
