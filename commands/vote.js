08c384bbe757f53476b6bdae7a91cd4b4be8a76a
jimlarck <jimlarck@gmail.com>
2019-02-20 10:19:28 -0600

    dynamic command handling, start on vote
    setup index to handle any commands found in ./commands and begin work on !vote. TODOs listed in vote.js

---
+++ commands/vote.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'vote',
+  description: 'sets up votes!',
+  execute(message, args) {
+    id = args[0]; // TODO: make sure id = int, return error otherwise
+    topic = args[1]; // TODO: make args =/= 0 be the topic (or think of better args for !vote)
+    console.log("ID: " + id);
+    console.log("topic: " + topic);
+
+    message.channel.send(topic).then(function (message){ 
+      message.react("👍")
+      message.react("👎")
+    });
+  },
+};
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+// Declare const/vars
 const Discord = require('discord.js');
+const fs = require("fs");
 const client = new Discord.Client();
 const config = require("./config.json")
 const keys = require("./private.json");
@@ -7,12 +9,39 @@ const keys = require("./private.json");
 let token = keys.token;
 var prefix = config.prefix;
 
-  // Return status
-  client.once("ready", () => {
-    console.log("Ready!");
-  });
+// Read commands from command folder
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync("./commands").filter(file => file.endsWith('.js'));
 
-  client.login(token)
+// populate commands from commandFile array
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
 
-  // RUN STUFF FROM COMMANDS HERE
-  
+// Return ready status
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+// Run commands
+client.on('message', message => {
+  const args = message.content.slice(prefix.length).split(/ +/);
+  const command = args.shift().toLowerCase();
+
+  console.log(command + " was executed!")
+
+  // Check for command received and execute appropriate command
+  if (!client.commands.has(command)) return;
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('there was an error trying to execute that command!');
+  }
+
+
+});
+
+// Send token
+client.login(token)
