d3f25555563e6d568b96a77ef7375bcc90868a8f
Mario García <mariogarcc@gmail.com>
2019-03-17 01:09:47 +0100

    !log can now fetch more than 50 messages at once
    Still have to clear+comment code, add datetime, improve performance, etc

--- commands/log.js
+++ commands/log.js
@@ -7,9 +7,14 @@ exports.run = async (message, args) => {
         channelName = message.channel.name
     }
 
-    let startID = args[1] == undefined ? null : args[1]
-    let endID = args[2] == undefined ? null : args[2]
+    let startID = args[1] == undefined ? null : args[1] // doesnt catch first
+    let endID = args[2] == undefined ? null : args[2] // catches last
 
+    // datetime_regex = /([0-3][0-9])\-([01][012])\-(20[12][0-9])/ // DDMMYYYY
+    // // SnowFlake is YYYY-MM-DDTHH:MM:SS:FFFZ
+    // startDateGroups = startDate.match(datetime_regex)
+
+    //bot.guilds.find(guildName => guildName.id == myguildidwashere
     let theChannel = await message.guild.channels.find(
         c => c.name == channelName && c.type == 'text')
 
@@ -18,14 +23,44 @@ exports.run = async (message, args) => {
         .catch(console.error)
     ).map(msg => ({ [msg.id]: [msg.author.username, msg.content] }))
 
-    let logFile = await JSON.stringify(messageLogs, null, 2)
+    let fetchComplete = (logs, end) => {
+        return (Object.keys(logs[0])[0] == end)
+    }
+
+    async function continueFetching(current) {
+        let currentID = Object.keys(current[0])[0]
+        let moreLogs = (await theChannel.fetchMessages({ after: currentID })
+            .then(logs => logs.filter(msg => msg.id <= endID))
+            .catch(console.error)
+        ).map(msg => ({ [msg.id]: [msg.author.username, msg.content] }))
+
+        let extendedLogs = moreLogs.concat(current)
+
+        if (!fetchComplete(extendedLogs, endID)) {
+            console.log('Continuing fetches...')
+            console.log(`Current number of messages: \
+                ${Object.keys(extendedLogs).length}`)
+            if (Object.keys(extendedLogs).length > 5000) { return extendedLogs }
+            return continueFetching(extendedLogs)
+        } else {
+            console.log('Finished fetching!')
+            return extendedLogs
+        }
+    }
+
+    var logs = messageLogs
+    if (!fetchComplete(logs, endID)) {
+        logs = (await continueFetching(logs))
+    }
+
+    let logFile = await JSON.stringify(logs, null, 2)
 
     await fs.writeFileSync(`${channelName}.json`, logFile, (err) => {
         if (err) throw err
         console.log('file saved')
     })
 
-    let numberOfMessages = Object.keys(messageLogs).length
+    let numberOfMessages = Object.keys(logs).length
     let fileStats = fs.statSync(`${channelName}.json`, (err) => {
         if (err) throw err
     })
